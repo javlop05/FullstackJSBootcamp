94a995340e3af83dd9df7a40d1a3a67b4adf90d9
javi <javilopezdelerma84@gmail.com>
2021-11-22 18:55:03 +0100

    feat: update note & filter by important

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import { useState, useEffect } from 'react';
 import Note from './components/Note';
 import { getAll, create, update } from './services/notes';
@@ -7,6 +8,7 @@ export default function App() {
   const [newNote, setNewNote] = useState('');
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(false);
+  const [showAll, setShowAll] = useState(true);
 
   useEffect(() => {
     setLoading(true);
@@ -42,19 +44,44 @@ export default function App() {
     setNewNote('');
   };
 
+  const updateNote = (id) => {
+    const noteToUpdateIndex = notes.findIndex((note) => note.id === id);
+    const noteToUpdate = notes[noteToUpdateIndex];
+    const newNote = {...noteToUpdate, important: !noteToUpdate.important };
+    
+    update(id, newNote)
+      .then(newNote => {
+        setNotes((prevNotes) => {
+          prevNotes[noteToUpdateIndex] = newNote;
+          return [...prevNotes];
+        });
+      })
+      .catch((error) => {
+        setError(error);
+      });
+  }
+
+  const notesToShow = showAll ? notes : notes.filter(note => note.important);
+
   return (
     <div>
       <h1>Notes</h1>
       {loading ?
         'Loading notes...' :
-        <ol>
-          {notes.map(note =>
-            <Note
-              key={note.id}
-              content={note.content}
-              important={note.important} />
-          )}
-        </ol>
+        <React.Fragment>
+          <button onClick={() => setShowAll(!showAll)}>
+            {showAll ? 'Show important' : 'Show all' }
+          </button>
+          <ol>
+            {notesToShow.map(note =>
+              <Note
+                key={note.id}
+                content={note.content}
+                important={note.important}
+                onClick={() => updateNote(note.id)}/>
+            )}
+          </ol>
+        </React.Fragment>
       }
 
       <form onSubmit={handleSubmit}>
